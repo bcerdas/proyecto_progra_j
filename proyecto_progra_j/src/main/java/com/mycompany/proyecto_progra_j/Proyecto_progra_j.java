2d60312999753745e53410c9764a4cb69fd6c21c
bcerdas <bryan.cerdas07@gmail.com>
2026-03-26 18:55:12 -0600

    first commit

---
+++ proyecto_progra_j/src/main/java/com/mycompany/proyecto_progra_j/Proyecto_progra_j.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_progra_j;
+
+/**
+ *
+ * @author bryan
+ */
+public class Proyecto_progra_j {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto Final!");
+    }
+}
